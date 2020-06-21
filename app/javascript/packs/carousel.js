87ab7c02496c1ca0dcaa6fc96ebd79bb252439c1
Adam Hanna <adamhanandev@gmail.com>
2020-06-20 21:13:02 -0700

    Add logo and slideshow to landing page

---
+++ app/javascript/packs/carousel.js
@@ -0,0 +1,16 @@
+let images = document.getElementsByClassName('image');
+console.log(images);
+let current = 0;
+
+setInterval(function () {
+    images[current].classList.remove("active");
+    current++;
+    if (current === images.length) {
+        current = 0;
+    }
+    images[current].classList.add("active");
+    console.log(`Current image id is - ${current}`);
+}, 10000);
+
+
+
