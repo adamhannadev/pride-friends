d542ed60e87181b5b3aa9fa96a501ecea3cc86b8
Adam Hanna <adamhanandev@gmail.com>
2020-07-03 13:09:04 -0700

    Justify all text left

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
     layout "main"
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
     def after_sign_in_path_for(resource)
         stored_location_for(resource) || connections_path
       end
-      
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      end
 end
