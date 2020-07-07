549335c0c6f65061f60601d7c6cc2ab8be0381d7
Adam Hanna <adamhanandev@gmail.com>
2020-07-07 12:02:06 -0700

    Seed the database with dummy data for the heroku nightly reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(:name => 'Demo User',
+            :email => 'test@example.com',
+            :password => 'demouser',
+            :password_confirmation => 'demouser')
+
+
+Connection.create([{
+    user: User.first,
+    names: 'James and John',
+    connection_date: Time.now,
+    insta: '@jamesandjohn',
+    memory: 'We met James and John at the drag ball game. They had a lovely dog named sophie and they are really into permaculture.'
+},{
+    user: User.first,
+    names: 'Elsie',
+    connection_date: Time.now,
+    insta: '@elsiewashere',
+    memory: 'Elsie has fabulous energy. She roasts her own coffee beans and volunteers for the NDP.'
+
+}
+])
